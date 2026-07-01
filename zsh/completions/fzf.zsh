714ee5920e8352ec6ab9a65222bc3debfa7fd8d5
lohit-dev <lohitsaidev@gmail.com>
2026-07-01 22:47:17 +0530

    refactor: extract zsh configuration into submodule

--- zsh/completions/fzf.zsh
+++
@@ -1,6 +0,0 @@
-    # Ctrl-R fzf completion
-    # Commented out because fzf is not installed
-    # To enable, install fzf: brew install fzf
-    # if command -v fzf &>/dev/null; then
-    #     eval "$(fzf --zsh)"
-    # fi
